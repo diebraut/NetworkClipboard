5156e903ef90fac8ab53ebbd20f6c8b67d79d8cc
Roland Prinz <rolandprinz@Mac-von-Roland.local>
2026-06-09 01:43:24 -0700

    Last changes

--- ios/src/ClipboardBridge.cpp
+++ ios/src/ClipboardBridge.cpp
@@ -2,6 +2,8 @@
 
 #include <QClipboard>
 #include <QGuiApplication>
+#include <QMimeData>
+#include <QTextDocument>
 
 namespace {
 QString withUnixLineEndings(QString text)
@@ -9,6 +11,23 @@ QString withUnixLineEndings(QString text)
     text.replace(QStringLiteral("\r\n"), QStringLiteral("\n"));
     text.replace(QLatin1Char('\r'), QLatin1Char('\n'));
     return text;
+}
+
+QString clipboardPlainText()
+{
+    const QMimeData *mimeData = QGuiApplication::clipboard()->mimeData();
+    if (!mimeData)
+        return {};
+
+    if (mimeData->hasHtml()) {
+        QTextDocument document;
+        document.setHtml(mimeData->html());
+        const QString text = document.toPlainText();
+        if (!text.trimmed().isEmpty())
+            return text;
+    }
+
+    return mimeData->text();
 }
 }
 
@@ -20,7 +39,7 @@ ClipboardBridge::ClipboardBridge(QObject *parent)
 
 QString ClipboardBridge::text() const
 {
-    return QGuiApplication::clipboard()->text();
+    return withUnixLineEndings(clipboardPlainText());
 }
 
 void ClipboardBridge::setText(const QString &text)
